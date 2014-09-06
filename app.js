e1b7c6ef6321f06a13c0a32f1bacda7443ac5f5f
Anthony Garcia-Labiad <anthony.garlab@gmail.com>
2014-09-06 20:30:40 +1000

    add compress for static content

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var compression = require('compression');
+
 var app = express();
 
 /*
@@ -6,7 +8,11 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });*/
 
-app.use(express.static(__dirname + '/public'));
+var oneDay = 86400000;
+
+app.use(compression({threshold: 512}));
+
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
